f07c074067599f21874bcb896117b27b64ab95ad
liuxinwen <1430968282@qq.com>
2017-09-18 20:04:59 +0800

    登陆页改写

--- mybxg/public/js/common.js
+++ mybxg/public/js/common.js
@@ -6,3 +6,30 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+
+	$('#logout').click(function () {
+		$.ajax({
+			type: 'post',
+			url: '/api/logout',
+			dataType: 'json',
+			success: function(data) {
+				if(data.code == 200) {
+					location.href = '/login';
+				}
+			}
+		})
+	})
+
+	var flag = $.cookie('PHPSESSID');
+	if(!flag) {
+		//如果cookie不存在，跳转到登陆页
+		location.href = '/main/login';
+	}
+
+	//将用户信息存储到index页面
+	var loginInfo = $.cookie('loginInfo');
+	loginInfo = loginInfo && JSON.parse(loginInfo);
+
+	$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	$('.aside .profile h4').html(loginInfo.tc_name);
